6a82e70efb19ce164c30068d0c90e5e33598cc53
Evan McCoy <evan.mccoy@gmail.com>
2019-08-20 22:12:18 -0400

    code cleanup and adding seed data

---
+++ api/sql/schema.sql
@@ -0,0 +1,11 @@
+create table invoices
+(
+  invoice_number integer primary key,
+  total numeric(9,2),
+  currency varchar(3),
+  invoice_date date,
+  due_date date,
+  vendor_name varchar(200),
+  remittance_address varchar(200),
+  status varchar(20)
+);
